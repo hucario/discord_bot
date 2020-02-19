8f78dc23de800402f8e106f457925200514802c2
hucario <50851047+hucario@users.noreply.github.com>
2020-02-18 19:58:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ client.on('ready', () => {
   client.channels.get(debugChannel).send('I RISE')
 });
 
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ', err);
+	client.channels.get(debugChannel).send('Caught exception: '+err)
+});
+
 client.login(process.env.TOKEN);
 
 process.on('exit', (code) => {
@@ -19,6 +24,13 @@ client.on('message', msg => {
       msg.reply('Pong!');
     }
     if (msg.content == 'crash') {
-        asdfe;
+        undefinedFunction();
     }
   });
+
+ require('http').createServer(function (request, response) {
+		response.writeHead(404);
+		response.end();
+ }).listen(process.env.PORT);
+
+ console.log(`Bound to port ${process.env.PORT} so that Heroku won't shut the app down.`);
