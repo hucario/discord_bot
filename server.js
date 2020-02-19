d3b2115eeabd6f489a83cd773eb9041edf5416b6
hucario <50851047+hucario@users.noreply.github.com>
2020-02-18 20:05:10 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var debugChannel = '679494924265455621';
+var errorSnarks = [
+	"That's an error.",
+	"FIRE IN THE SERVER ROOM!",
+	"Whoopsie.",
+	"did you do this??",
+	"This is all your fault.",
+	"oh, kill me. now I have to debug."
+	];
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,6 +20,14 @@ process.on('uncaughtException', function (err) {
   console.log('Caught exception: ', err);
 	client.channels.get(debugChannel).send('Caught exception: '+err)
 });
+
+function err(error,replychannel) {
+	console.log(error);
+	if (replychannel) {
+		replychannel.send(`\n${errorSnarks[Math.floor(Math.random()*errorSnarks.length)]}\`\`\`javascript\n${err}`);
+	}
+	client.channels.get(debugChannel).send(error);
+}
 
 client.login(process.env.TOKEN);
 
